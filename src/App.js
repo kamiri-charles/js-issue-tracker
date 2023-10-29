3b3c2a76cd18e568e48fed313d0bb7ef778b19d3
Charles Kamiri <charleskamiri6@gmail.com>
2023-10-29 22:19:35 +0300

    Added boxicons package and fonts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import SignIn from './components/SignIn';
+import 'boxicons/css/boxicons.min.css';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/sign-in" element={ <SignIn />} />
+          <Route path='/' element={ <Home />} />
+          <Route path='/sign-in' element={ <SignIn />} />
         </Routes>
       </Router>
     </div>
