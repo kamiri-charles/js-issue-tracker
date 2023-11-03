9a45129c166f67e12eafa6a453967cc1541263be
Charles Kamiri <charleskamiri6@gmail.com>
2023-11-03 22:45:32 +0300

    Sign in redirect bug fix

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,17 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './styles.scss';
-import { useState } from 'react';
 
 const Dashboard = () => {
 
     let nav = useNavigate();
-
     let [userData, setUserData] = useState();
 
     useEffect(() => {
         // Check if user is logged in
-        if (!JSON.parse(localStorage.getItem('jit_user_data')).id) {
+        if (localStorage.getItem('jit_user_data') === null) {
             // Redirect to sign in page
             nav('/sign-in');
         } else {
